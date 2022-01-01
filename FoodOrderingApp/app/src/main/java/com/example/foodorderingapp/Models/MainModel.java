a9dead5ea22dae0e624d23f639231f6889a195f3
itianchamp <itianchamp@gmail.com>
2022-01-01 19:38:52 +0500

    fourth commit

--- FoodOrderingApp/app/src/main/java/com/example/foodorderingapp/Models/MainModel.java
+++ FoodOrderingApp/app/src/main/java/com/example/foodorderingapp/Models/MainModel.java
@@ -5,6 +5,29 @@ public class MainModel {
     int image;
     String name, price, description;
 
+    public MainModel(int image, String name, String price, String description) {
+        this.image = image;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getPrice() {
         return price;
     }
@@ -20,31 +43,4 @@ public class MainModel {
     public void setDescription(String description) {
         this.description = description;
     }
-
-    public MainModel(int image, String name, String price, String description) {
-        this.image = image;
-        this.name = name;
-        this.price = price;
-        this.description = description;
-    }
-
-
-
-    public int getImage() {
-        return image;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setImage(int image) {
-        this.image = image;
-    }
-
-
 }
